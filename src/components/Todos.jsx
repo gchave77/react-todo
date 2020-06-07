b07dbd809bc50aa0d8020ec84779de97e7152822
gabe chavez <gabe.chavez77@gmail.com>
2020-06-06 19:47:38 -1000

    react setup

---
+++ src/components/Todos.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+function todos() {
+  return (
+    <div>
+      <h1>Gabe's To Do App (from Todos.jsx)</h1>
+    </div>
+  );
+}
+
+export default todos;
